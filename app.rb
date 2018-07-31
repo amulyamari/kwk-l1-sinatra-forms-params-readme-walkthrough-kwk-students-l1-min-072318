f8fc256b24480100e23a7444781538e9aa701d07
amulyamari <amulya@saimail.com>
2018-07-31 19:37:24 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,8 +3,13 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/food_form' do
-    
-
-  # Add your post route and action below
-
+    erb: 'index.html'
+  end
+  
+  get ''
+  <form method="POST" action="/food">
+  <p>Your Name: <input type="text" name="name"></p>
+  <p>Your Favorite Food: <input type="text" name="favorite_food"></p>
+  <input type="submit">
+  </form>
 end
