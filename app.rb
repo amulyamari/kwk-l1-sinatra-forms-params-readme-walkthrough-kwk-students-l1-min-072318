583592d872c1083f4be62322b3cb82b49b918440
amulyamari <amulya@saimail.com>
2018-07-31 19:34:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,10 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/food_form' do
-    erb :food_form
-    <input 
-    type="submit">
-  end
+    
 
   # Add your post route and action below
 
